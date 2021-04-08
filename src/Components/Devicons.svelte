b7f0cb614ada9d4cbc6252e610609111aae76a9f
Alex Wiegand <72582154+Wiegand92@users.noreply.github.com>
2021-04-08 16:48:57 -0400

    migrating styles to tailwind

--- src/Components/Devicons.svelte
+++ src/Components/Devicons.svelte
@@ -20,36 +20,22 @@
 </section>
 
 <style>
-  section{
-    padding:none
-  }
   .container__devicon-container{
-    display: grid;
-    grid-template: 
-        [col-1-left] "d b d b d" [col-1-right] 
-        [col-2-left] "b d b d b" [col-2-right] 
-        [col-3-left] "d b d b d" [col-3-right];
-    grid-template-rows: 1fr 1fr 1fr;
-    grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
     width: 50%;
     height: 30%;
     border: .4rem ridge #F4A15D;
-    margin: auto;
-    padding: 0
+
+    @apply grid grid-flow-col grid-rows-3 m-auto p-0;
   } 
   
   .devicon {
-    background-color: #1D5962 ;
-    box-sizing: content-box;
-    color: #F4A15D;
-    grid-area: "d";
     font-size: 200%;
-    text-align: center;
     padding: 25% 0 25% 0;
+
+    @apply bg-blue-light text-orange-light box-content text-center;
   }
 
   .background {
-    grid-area: 'b';
     background-color: rgba(255, 255, 255, 0.37)
   }
 

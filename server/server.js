71eab050d00856494973f485b55debb4cf0e68a6
wiegand92 <wiegand.alex@gmail.com>
2021-04-28 14:18:22 -0400

    add netlify dependencies

--- server/server.js
+++ server/server.js
@@ -3,12 +3,13 @@ const app = express();
 const cors = require('cors');
 const session = require('express-session');
 const passport = require('passport');
+const serverless = require('serverless-http');
 require('dotenv').config();
 
 // Route imports //
 
-const projects = require('./server/routes/projects');
-const authenticate = require('./server/routes/authenticate')
+const projects = require('./routes/projects');
+const authenticate = require('./routes/authenticate')
 
 // If we deploy, will set the port to whatever server, else 4200 //
 
@@ -28,17 +29,15 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 
-//Serve static files//
-
-app.use(express.static(__dirname + '/public'));
-
 // Projects Routes //
-app.use('/projects', projects);
+app.use('/.netlify/functions/projects', projects);
 
 // User Routes //
-app.use('/authenticate', authenticate);
+app.use('/.netlify/functions/authenticate', authenticate);
 
 // Start Server //
 app.listen(PORT, () => {
   console.log(`server started on port: ${PORT}`)
 });
+
+module.exports.handler = serverless(app)
